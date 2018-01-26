c0d700fdc78f570a87782619c915801b8173b6e4
ilya <n1ro0ily@gmail.com>
2018-01-26 12:34:28 +0200

    Adds new methods to Tweet and Hashtag querysets.

--- twitterapiwrapper/trends/querysets.py
+++ twitterapiwrapper/trends/querysets.py
@@ -9,9 +9,11 @@ class TrendQuerySet(models.QuerySet):
 
 class TweetQuerySet(models.QuerySet):
     def without_hashtags(self):
-        return self.all()
+        return self.annotate(hashtags_count=models.Count('hashtags')).filter(hashtags_count=0)
     without_hashtags.queryset_only = False
 
 
 class HashtagQuerySet(models.QuerySet):
-    pass
+    def popular(self):
+        return self.annotate(tweets_count=models.Count('tweets')).filter(tweets_count__gte=40)
+    popular.queryset_only = False
